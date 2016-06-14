3b432cf19d6134c7cddb5bcd014ec5374d1b955d
jonathanaumann <jon.8998@hotmail.com>
2016-06-14 11:51:55 -0400

    added speeded and more methods

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,26 @@
+#lib/car.rb
 class Car 
-	def drive
-	puts "BRoooom"
+	attr_reader :type
+	def initialize 
+		@type = type 
+
 	end
+
+	def drive
+		if type == "noisy_car"
+			puts "BROOOOM"
+		end
+		if type == "normal_car"
+			puts "broom"
+		end
+	end
+
+	def speed_control 
+		puts "how fast are you going?"
+		speed= gets.chomp.to_i
+		if speed > 100
+			puts "damn slow down"
+		end
+	end
+
 end
