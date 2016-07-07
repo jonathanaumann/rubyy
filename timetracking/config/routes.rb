396016fb4daa577cfbb45e4b4f54b90a2002ff55
jonathanaumann <jon.8998@hotmail.com>
2016-07-07 15:25:41 -0400

    finished all features and refactored routes

--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -1,26 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get '/', to: 'site#home'
+	get '/', to: 'site#home'
 
-get "/projects", to: "projects#index" 
+	resources :projects, only: [:index, :show, :new, :create] do 
+	resources :time_entries, except: [:show]
+		end
 
-
-get "/project/new", to: "projects#new"
-get "/project/:id", to: "projects#show"
-
-
-post "/projects" => "projects#create"
-
-
-
-get "/projects/:project_id/time_entries", to: 'time_entries#show'
-
-get "/projects/:project_id/time_entries/new", to: "time_entries#new"
-
-post "/projects/:project_id/time_entries", to: "time_entries#create", as: :project_time_entries
-
-get "/projects/:project_id/time_entries/:id/edit", to: "time_entries#edit"
-
-patch '/projects/:project_id/time_entries/:id', to: "time_entries#update", as: :project_time_entry
 end
